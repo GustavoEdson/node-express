a7806278132ede6066c2a3c4e37c2963c43f9ee0
Gustavo Edson <ggugu9289@gmail.com>
2024-01-20 12:26:57 -0300

    params

--- src/index.mjs
+++ src/index.mjs
@@ -4,18 +4,31 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id: 1, username: "John Doe", age: 25, email: "john@example.com" },
+  { id: 2, username: "Jane Smith", age: 30, email: "jane@example.com" },
+  { id: 3, username: "Bob Johnson", age: 22, email: "bob@example.com" },
+  { id: 4, username: "Alice Williams", age: 28, email: "alice@example.com" },
+  { id: 5, username: "Charlie Brown", age: 32, email: "charlie@example.com" },
+];
+
 app.get("/", (req, res) => {
   res.status(201).send({ mgs: "Hello, world" });
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, username: "John Doe", age: 25, email: "john@example.com" },
-    { id: 2, username: "Jane Smith", age: 30, email: "jane@example.com" },
-    { id: 3, username: "Bob Johnson", age: 22, email: "bob@example.com" },
-    { id: 4, username: "Alice Williams", age: 28, email: "alice@example.com" },
-    { id: 5, username: "Charlie Brown", age: 32, email: "charlie@example.com" },
-  ]);
+  res.send(mockUsers);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  console.log(req.params.id);
+  const parsedId = parseInt(req.params.id);
+  if (isNaN(parsedId))
+    return res.status(400).send({ msg: "Bad request, Invalid ID" });
+
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return res.status(404).send({ msg: "User not Found" });
+  return res.send(findUser);
 });
 
 app.get("/api/products", (req, res) => {

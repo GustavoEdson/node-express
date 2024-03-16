0b8d5e994ff1411f7dfd6e8535dfa7c1855d1f9a
Gustavo Edson <ggugu9289@gmail.com>
2024-03-16 13:21:30 -0300

    update

--- src/index.mjs
+++ src/index.mjs
@@ -1,39 +1,28 @@
 import express, { response } from "express";
 import cors from "cors";
-
 const app = express();
 
-app.use(cors());
-
-const PORT = process.env.PORT || 3000;
-
-const mockUsers = [
-  { id: 1, username: "John Doe", age: 25, email: "john@example.com" },
-  { id: 2, username: "Jane Smith", age: 30, email: "jane@example.com" },
-  { id: 3, username: "Bob Johnson", age: 22, email: "bob@example.com" },
-  { id: 4, username: "Alice Williams", age: 28, email: "alice@example.com" },
-  { id: 5, username: "Charlie Brown", age: 32, email: "charlie@example.com" },
-];
+const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.status(201).send({ msg: "Hello!" });
+  res.send(`
+  <form action="" method="POST">
+  Nome: <input type="text" name="name">
+  <button>Enviar</button>
+  </form>
+  `);
 });
 
-app.get("/api/users", (req, res) => {
-  res.send(mockUsers);
+app.post("/", (req, res) => {
+  res.send("Recebi o formulario");
 });
 
-app.get("/api/users/:id", (req, res) => {
-  const parsedId = parseInt(req.params.id);
-  if (isNaN(parsedId)) return res.sendStatus(400);
-
-  const findUser = mockUsers.find((user) => user.id === parsedId);
-  if (!findUser) return res.sendStatus(404);
-  return res.send(findUser);
+app.get("/contato", (req, res) => {
+  res.send("Obrigado por entrar em contato com a gente!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 // localhost:3000

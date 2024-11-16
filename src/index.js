f1e42ad35e1319970f1c6a997f54a517cb3a2ecd
GustavoEdson <ggugu9289@gmail.com>
2024-11-15 21:21:14 -0300

    update

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const app = express()
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.status(201).send({msg:"Hello"})
+})
+
+app.get("/api/products", (req, res))
+
+app.get("/api/users", (req, res) => {
+    res.statusCode(201).send([
+    {user: "Pedro",  id: "423gvh23k43242"},
+    {user: "Gustavo", id: "423gvh23k43242"},
+    {user: "Alan", id: "423gvh23k43242"},
+    ])
+})
+
+app.get("/api/products", (req, res) => {
+    res.send([
+        {id: 125, name:"chicken breast", price: 12.99},
+        {id: 125, name:"chicken breast", price: 12.99},
+        {id: 125, name:"chicken breast", price: 12.99}])
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on Port ${PORT}`);
+})

b1ea03f6bbac0cb52515d1dd8a5aceb91e82cdaf
GustavoEdson <ggugu9289@gmail.com>
2024-11-17 18:09:05 -0300

    update

--- src/index.js
+++ src/index.js
@@ -4,22 +4,35 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+    {user: "Pedro",  id: 1},
+    {user: "Gustavo", id: 2},
+    {user: "Alan", id: 3},
+    ]
+
 app.get("/", (req, res) => {
     res.status(201).send({msg:"Hello"})
 })
 
-
 app.get("/api/users", (req, res) => {
-    res.statusCode(201).send([
-    {user: "Pedro",  id: "423gvh23k43242"},
-    {user: "Gustavo", id: "423gvh23k43242"},
-    {user: "Alan", id: "423gvh23k43242"},
-    ])
+    res.send(mockUsers)
 })
 
+app.get("/api/users/:id", (req, res) => {
+    // console.log(req.params.id); 
+    const parsedId = parseInt(req.params.id)
+    // console.log(parsedId);
+    if (isNaN(parsedId)) 
+        return res.status(400).send({msg: "Bad request. Invalid ID"})
 
-app.get("/api/games", (req, res) => {
-    res.send("hi there your game library is empty")
+    const findUser = mockUsers.find((user) => user.id === parsedId)
+    if (!findUser) return res.sendStatus(404)
+        return res.send(findUser)
+    
+})
+
+app.get("/api/products/", (req, res) => {
+    res.send([{id: 123, name: "chicken breast", price: 12.99}])
 })
 
 app.listen(PORT, () => {
